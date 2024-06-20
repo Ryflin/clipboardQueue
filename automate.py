4189184bb105d3a4eb7d70506340a278c91c894c
Ryflin <Rydogger729@gmail.com>
2024-06-20 17:01:26 -0400

    going based on inputs instead of id (way more extensible)

--- automate.py
+++ automate.py
@@ -14,46 +14,28 @@ try:
     queue = json.load(f)
 except:
   print("It looks like there is nothing in the input queue\n")
-
-  qu = {"value": input("Enter an item, press q then ENTER to exit: "), "id": input("Enter the id of the item: ")}
-  while qu["value"] != "q" and qu["id"] != "q":
+  qu =  input("Enter the next item: ")
+  while qu != "q":
     queue.append(qu)
-    qu = {"value": input("Enter an item, press q then ENTER to exit: "), "id": input("Enter the id of the item: ")}
+    qu = input("Enter an item, press q then ENTER to exit: ")
   json.dump(queue, open("automate.json", "w"))
 
 print(queue)
 def fill_form2():
   global queue, ids
   web = webdriver.Firefox()
-  site = web.get("https://www.ohiopilotexaminer.com/booking-calendar/cfi-amel-asel-added-class?referral=service_list_widget")
+  site = web.get("http://127.0.0.1:5500/formSite/index.html")
   while "booking-form" not in web.current_url:
     time.sleep(.1)
   time.sleep(.5)
-
-  try:
-    elements = web.find_elements(By.CLASS_NAME, "sk7DQ7V")
-    if len(elements) > 2:
-      elements[2].send_keys("+1")
-  except Exception as e:
-    print(e, '\n')
-    pass
-  for i in range(0, len(queue)):
-    web.find_element(By.ID, queue[i]["id"]).send_keys(queue[i]["value"])
-  # try the two checkboxes
-  try:
-    checkbox = web.find_element(By.ID, "checkbox-160")
-    checkbox.send_keys(" ")
-  except Exception as e:
-    print(e, '\n')
-    pass
-  try:
-    web.find_element(By.ID, "checkbox-177").send_keys(" ")
-  except Exception as e:
-    print(e, '\n')
-    pass
-  
+  elements = web.find_elements(By.TAG_NAME, "input")
+  for i in range(0, len(elements)):
+    try:
+      elements[i].send_keys(queue[i])
+    except Exception as e:
+      print(e, '\n')
+      pass
 fill_form2()  
-
 user_input = input("Press s to start listener for p, q to quit: ")
 while user_input != "q":
   # listener.start()
